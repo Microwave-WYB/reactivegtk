271134f641836dbab6182ab466487031716163ab
Yibo Wei <david_wyb2001@outlook.com>
2025-06-08 13:34:22 -0700

    Add type ignore and noqa comment to gi.repository import

--- demos/hello.py
+++ demos/hello.py
@@ -2,7 +2,7 @@ import gi
 from reactivegtk import MutableState, WidgetLifecycle, into
 
 gi.require_versions({"Gtk": "4.0", "Adw": "1"})
-from gi.repository import Gtk, Adw
+from gi.repository import Gtk, Adw  # type: ignore # noqa: E402
 
 
 def HelloWorld():
