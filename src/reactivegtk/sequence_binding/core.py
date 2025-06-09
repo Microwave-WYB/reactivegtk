0e5acf9de0f9002da14eac8d9d4a6e1d87875069
Yibo Wei <david_wyb2001@outlook.com>
2025-06-08 23:25:58 -0700

    refactor: add cleanup on widget removal

--- src/reactivegtk/sequence_binding/core.py
+++ src/reactivegtk/sequence_binding/core.py
@@ -4,7 +4,7 @@ from typing import Any, Callable, TypeVar, overload
 
 import gi
 
-from reactivegtk.lifecycle import watch
+from reactivegtk.lifecycle import watch, cleanup
 from reactivegtk.sequence_binding._diff import diff_update
 from reactivegtk.state import State
 
@@ -53,90 +53,54 @@ def bind_sequence(
     """Bind a sequence state to a GTK container using efficient diff updates."""
 
     def decorator(item_factory: Callable[[ItemT], Any]) -> None:
-        # Keep track of current state
-        current_items: Sequence[ItemT] = tuple()
+        # Use a dict to store mutable state
+        state = {"current_items": tuple(), "widget_by_key": {}}
 
-        # Container adapter to interface with diff_update
-        class ContainerAdapter:
-            def __init__(self, container: Gtk.Widget):
-                self.container = container
-                self._widget_by_key: dict[KeyT, Gtk.Widget] = {}
+        def get_container_widgets() -> Iterator[Gtk.Widget]:
+            """Get current widgets in container in order."""
+            child = container.get_first_child()
+            while child:
+                yield child
+                child = child.get_next_sibling()
 
-            def get_items(self) -> Iterator[Gtk.Widget]:
-                """Get current widgets in order."""
-                child = self.container.get_first_child()
-                while child:
-                    yield child
-                    child = child.get_next_sibling()
+        def remove_widget_from_container(widget: Gtk.Widget) -> None:
+            """Remove widget from container and clean up tracking."""
+            remove_widget(container, widget)
+            cleanup(widget)
 
-            def remove_widget(self, widget: Gtk.Widget) -> None:
-                """Remove widget from container."""
-                remove_widget(self.container, widget)
+            # Remove from tracking dict
+            for key, tracked_widget in list(state["widget_by_key"].items()):
+                if tracked_widget is widget:
+                    del state["widget_by_key"][key]
+                    break
 
-                # Remove from our tracking dict
-                key_to_remove = None
-                for key, tracked_widget in self._widget_by_key.items():
-                    if tracked_widget is widget:
-                        key_to_remove = key
-                        break
-                if key_to_remove is not None:
-                    del self._widget_by_key[key_to_remove]
+        def insert_widget_in_container(widget: Gtk.Widget, position: int) -> None:
+            """Insert widget at position in container."""
+            insert_widget_at(container, widget, position)
 
-            def insert_widget(self, widget: Gtk.Widget, position: int) -> None:
-                """Insert widget at position."""
-                insert_widget_at(self.container, widget, position)
-
-            def create_widget(self, item: ItemT) -> Gtk.Widget:
-                """Create and track a new widget for an item."""
-                widget = item_factory(item)
-                key = key_fn(item)
-                self._widget_by_key[key] = widget
-                return widget
-
-            def get_widget_for_key(self, key: KeyT) -> Gtk.Widget | None:
-                """Get tracked widget for a key."""
-                return self._widget_by_key.get(key)
-
-            def track_widget(self, key: KeyT, widget: Gtk.Widget) -> None:
-                """Track a widget by its key."""
-                self._widget_by_key[key] = widget
-
-        adapter = ContainerAdapter(container)
+        def create_and_track_widget(item: ItemT) -> Gtk.Widget:
+            """Create widget and track it by key."""
+            widget = item_factory(item)
+            state["widget_by_key"][key_fn(item)] = widget
+            return widget
 
         def sync_items():
             """Sync container using efficient diff algorithm."""
-            nonlocal current_items
             new_items = tuple(items.value)
 
-            def widget_factory(item: ItemT) -> Gtk.Widget:
-                return adapter.create_widget(item)
-
-            def find_widget_by_key(
-                widgets: Sequence[Gtk.Widget],
-                old_items: Sequence[ItemT],
-                target_key: KeyT,
-            ) -> Gtk.Widget | None:
-                """Find widget corresponding to key in old items."""
-                for i, old_item in enumerate(old_items):
-                    if key_fn(old_item) == target_key and i < len(widgets):
-                        return widgets[i]
-                return None
-
-            # Use diff_update to efficiently sync the container
             diff_update(
-                container=adapter,
-                old_source=current_items,
+                container=None,  # We don't actually need this if we pass functions directly
+                old_source=state["current_items"],
                 new_source=new_items,
                 key_func=key_fn,
-                factory=widget_factory,
-                remove=lambda container_adapter, widget: container_adapter.remove_widget(widget),
-                insert=lambda container_adapter, widget, pos: container_adapter.insert_widget(widget, pos),
-                get_container_items=lambda container_adapter: tuple(container_adapter.get_items()),
+                factory=create_and_track_widget,
+                remove=lambda _container, widget: remove_widget_from_container(widget),
+                insert=lambda _container, widget, pos: insert_widget_in_container(widget, pos),
+                get_container_items=lambda _container: tuple(get_container_widgets()),
             )
 
-            current_items = new_items
+            state["current_items"] = new_items
 
-        # Set up the watch
         @watch(container, items, init=True)
         def _(_):
             sync_items()
@@ -152,7 +116,6 @@ def insert_widget_at(container: Gtk.Widget, widget: Gtk.Widget, index: int) -> N
 
 @insert_widget_at.register
 def _(container: Gtk.ListBox, widget: Gtk.Widget, index: int) -> None:
-    """Insert widget at index in ListBox."""
     if widget.get_parent() is not None:
         widget.unparent()
     container.insert(widget, index)
@@ -160,7 +123,6 @@ def _(container: Gtk.ListBox, widget: Gtk.Widget, index: int) -> None:
 
 @insert_widget_at.register
 def _(container: Gtk.Box, widget: Gtk.Widget, index: int) -> None:
-    """Insert widget at index in Box."""
     if index == 0:
         container.prepend(widget)
     else:
@@ -179,7 +141,6 @@ def _(container: Gtk.Box, widget: Gtk.Widget, index: int) -> None:
 
 @insert_widget_at.register
 def _(container: Gtk.FlowBox, widget: Gtk.Widget, index: int) -> None:
-    """Insert widget at index in FlowBox."""
     if widget.get_parent() is not None:
         widget.unparent()
     container.insert(widget, index)
@@ -193,7 +154,6 @@ def remove_widget(container: Gtk.Widget, widget: Gtk.Widget) -> None:
 
 @remove_widget.register
 def _(container: Gtk.ListBox, widget: Gtk.Widget) -> None:
-    """Remove widget from ListBox."""
     container.remove(widget)
     if widget.get_parent() is not None:
         widget.unparent()
@@ -201,7 +161,6 @@ def _(container: Gtk.ListBox, widget: Gtk.Widget) -> None:
 
 @remove_widget.register
 def _(container: Gtk.FlowBox, widget: Gtk.Widget) -> None:
-    """Remove widget from FlowBox."""
     container.remove(widget)
     if widget.get_parent() is not None:
         widget.unparent()
